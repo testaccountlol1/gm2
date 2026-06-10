214d9a8d23ba240186d7d0a0ef00b0986fe46538
testaccountlol1 <cavidanrzayev67@gmail.com>
2026-06-10 17:03:37 +0400

    made the baseline things for now,im gonna go on break a bit after this and also ive developed a lot of other games in the past few days

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,1 +1,24 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include <string>
+enum class GameState
+{
+    Menu,
+    Playing,
+    Paused,
+    GameOver
+};
+enum class Move
+{
+    Slash,
+    Stab,
+    Block,
+    Knife,
+    KillerCombo
+};
+std::vector<std::string> playerMoves;
+unsigned int mp = 0;
+int main() {
+    //add code here
+    return 0;
+}
